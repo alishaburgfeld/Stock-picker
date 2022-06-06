61f1ffb67c53ca588874e6ae4d5c148f93a6781d
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-06-06 15:08:37 -0400

    added JS nad PY

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,21 @@
-exports.picker = function(prices) {
-
+// exports.picker = function(list) {
+function picker(list) {
+    let min= Math.min(...list)
+    answer=[0]
+    for (let i=0;i<list.length-2;i++) {
+        for (let j=list.length-1;j>i;j--) {
+            if (list[j]-list[i] > min) {
+                min=list[j]-list[i]
+                answer[0]=[i,j]
+            }
+        }
+    }
+    return answer[0]
 }
+
+
+console.log(picker([17,3,6,9,15,8,6,1,10])) // , [1, 4]))
+console.log(picker([17,3,6,9,15,8,6,1,10])) //, [1,4]))
+console.log(picker([3,17,6,9,18,15,6,1,10])) //, [0,4]))
+console.log(picker([1,17,6,9,8,15,6,3,19])) //, [0,8]))
+console.log(picker([19,17,6,9,8,15,6,3,1])) //, [2,5]))
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,12 @@
 def picker(prices):
-    pass
+    difference=[0,0]
+    for i, x in enumerate(prices[:len(prices)-1]):
+        for j in range( len(prices) - 1, i, -1) :
+
+            if prices[j] - x > difference[0]:
+                difference[0]=prices[j] - x
+                difference[1]=[i,j]
+            # print (i,j, difference)
+    return difference[1]
+        
+# print(picker([17,3,6,9,15,8,6,1,10])) # == [1,4])
